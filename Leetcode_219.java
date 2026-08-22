db56d03250afb6e9dec0c2b2cebd31875fc1af69
aryan <aryan@gmail.com>
2026-08-22 15:33:52 +0530

    Contain Duplicates II

---
+++ Leetcode_219.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i]))
+                return true;
+
+            if (i >= k)
+                set.remove(nums[i - k]);
+        }
+
+        return false;
+    }
+} 
